92c12f57dd7d59934d92842b3ce54aacebd90f4d
Martin Bing <bing.martin.photography@gmail.com>
2017-11-09 23:32:00 +0100

    Add readme and firebase setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sample List project Ionic
+
+As this was made with Ionic you can use the following commands for a quick start.
+
+```
+	$ ionic lab // runs a livereload server
+```
+
+Update `firebase.credentials.ts` with your own credentials to get started.
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireDatabaseModule } from 'angularfire2';
+import { AngularFireModule } from 'angularfire2/database';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+IMPORT { FIREBASE_CONFIG } from './firebase.credentials'
 
 @NgModule({
   declarations: [
@@ -13,7 +15,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
